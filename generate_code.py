cb4dd6579f58848ebeebf4223da3d17c74853c15
David Kennedy S. A <daviduser.guj@gmail.com>
2018-04-13 14:08:02 -0300

    Flat global context

--- generate_code.py
+++ generate_code.py
@@ -7,16 +7,6 @@ import collections
 import argparse
 import copy
 
-parser = argparse.ArgumentParser(description='Generate text based on Mustache extended templates.')
-parser.add_argument('template', type=str, help='Path to template directory')
-parser.add_argument('data', type=str, help='Path to data used to fill based on templates')
-parser.add_argument('result', type=str, help='Path where the generate files or directories will be placed')
-
-args = parser.parse_args()
-template_path = args.template
-data_path = args.data
-result_path = args.result
-
 def yaml_loader(path, context = {}):
     if os.path.isfile(path):
         partial_document = yaml.load(open(path, 'r'))
@@ -44,15 +34,10 @@ def yaml_loader(path, context = {}):
 
     return context
 
-document = yaml_loader(data_path)
-print('\nUsing data: ')
-__import__('pprint').pprint(document)
-print('\n')
 
 mustache_pattern = re.compile(r'__([^_]+)__')
 
-def mustache_directory_apply(path, context, directory = copy.deepcopy(result_path)):
-    print(context)
+def mustache_directory_apply(path, context, directory):
     for resource in os.listdir(path):
         resource_path = os.path.join(path, resource)
         extended_mustache_name = mustache_pattern.search(resource)
@@ -77,6 +62,7 @@ def mustache_directory_apply(path, context, directory = copy.deepcopy(result_pat
                 new_directory = os.path.join(directory, generated_name)
                 mustache_directory_apply(resource_path, context_item, new_directory)
 
+
 def mustache_path_apply(path, context):
     final_path = []
     for resource in path.split('/'):
@@ -85,14 +71,14 @@ def mustache_path_apply(path, context):
 
     return os.path.join(*final_path)
 
+
 def solve_extended_mustache(resource, context):
     context = copy.deepcopy(context)
     mustache_on_file = mustache_pattern.search(resource)
-    last_context = None
     while mustache_on_file != None:
         extended_mustache_name = mustache_on_file.group(1)
         if extended_mustache_name != resource:
-            key, last_context = extended_mustache_solve_context(extended_mustache_name, document)
+            key, _ = extended_mustache_solve_context(extended_mustache_name, document)
             resource = resource.replace(mustache_on_file.group(), str(context[key]))
         mustache_on_file = mustache_pattern.search(resource)
     return resource 
@@ -104,7 +90,23 @@ def extended_mustache_solve_context(name, context):
     path = path[:-1]
     for resource in path:
         context = context[resource]
+
     return value, context
+
+
+def flat_globalize(context, global_context = dict(), prefix = ''):
+    if isinstance(context, dict):
+        for k, v in context.items():
+            global_context[prefix + '.' + k] = v
+
+        for k, v in context.items():
+            flat_globalize(v, global_context, prefix + '.' + k)
+
+    if isinstance(context, list):
+        for item in context:
+            flat_globalize(item, global_context, prefix)
+            item.update(global_context)
+
 
 def mustache_file_apply(template_path, generated_path, context):
     print('[Filling] \t\t' + template_path + ' => ' + generated_path)
@@ -114,4 +116,25 @@ def mustache_file_apply(template_path, generated_path, context):
     generatade_file_stream = open(generated_path, 'w')
     generatade_file_stream.write(generated)
 
-mustache_directory_apply(template_path, document)
+parser = argparse.ArgumentParser(description='Generate text based on Mustache extended templates.')
+parser.add_argument('template', type=str, help='Path to template directory')
+parser.add_argument('data', type=str, help='Path to data used to fill based on templates')
+parser.add_argument('result', type=str, help='Path where the generate files or directories will be placed')
+
+args = parser.parse_args()
+template_path = args.template
+data_path = args.data
+result_path = args.result
+
+document = yaml_loader(data_path)
+print('\nUsing data: ')
+__import__('pprint').pprint(document)
+print('\n')
+
+flat_globalize(document)
+
+print('\nUsing globalized data: ')
+__import__('pprint').pprint(document)
+print('\n')
+
+mustache_directory_apply(template_path, document, result_path)
